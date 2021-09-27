f62003384b5d3eb9416c0df46a56c08a84174868
mikesaraus <mikesaraus@github.com>
2021-09-28 01:07:40 +0800

    Auto status on IDLE

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,75 @@
 <template>
-  <router-view v-slot="{ Component }">
+  <router-view @mousemove="testIdle" v-slot="{ Component }">
     <component :is="Component" />
   </router-view>
 </template>
 <script>
-import { useQuasar } from "quasar";
+import { LocalStorage, useQuasar } from "quasar";
 import { defineComponent } from "vue";
-import { mapActions } from "vuex";
+import { mapActions, mapState } from "vuex";
 import { greetings } from "./assets/scripts/functions";
+import { mixinLogout } from "./mixins";
+import idleTimeout from "idle-timeout";
 
 export default defineComponent({
   name: "App",
 
+  mixins: [mixinLogout],
+
   data() {
     return {
       $q: useQuasar(),
+      idle: false,
+      idle_expire: 10000,
     };
+  },
+
+  computed: {
+    ...mapState("firebase_auth", ["userDetails"]),
+
+    isLogin() {
+      if (LocalStorage.has(this.$myvar.localStorage.userDetails)) return true;
+      return false;
+    },
+  },
+
+  created() {
+    greetings.console();
+    this.handleAuthStateChanged();
+
+    this.idle = idleTimeout(
+      async () => {
+        if (this.isLogin) {
+          await this.firebaseUpdateUser({
+            userId: this.userDetails.userId,
+            updates: { online: false },
+          });
+        }
+      },
+      {
+        element: window,
+        timeout: this.idle_expire,
+        loop: false,
+      }
+    );
   },
 
   methods: {
     ...mapActions("firebase_auth", [
       "handleAuthStateChanged",
       "firebaseUpdateCurrentUser",
+      "firebaseUpdateUser",
     ]),
-  },
 
-  created() {
-    greetings.console();
-    this.handleAuthStateChanged();
+    async testIdle(e) {
+      if (this.idle.idle && this.isLogin) {
+        await this.firebaseUpdateUser({
+          userId: this.userDetails.userId,
+          updates: { online: true },
+        });
+        this.idle.reset();
+      }
+    },
   },
 
   beforeMount() {
@@ -35,6 +77,10 @@ export default defineComponent({
       this.$myvar.localStorage.userDetails
     );
     if (userDetails) this.firebaseUpdateCurrentUser(userDetails);
+  },
+
+  beforeUnmount() {
+    this.idle.destroy();
   },
 });
 </script>
